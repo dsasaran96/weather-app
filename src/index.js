b604eec117a26a6b086609887c527eb49bc87eec
Dragos Sasaran <dragos.sasaran@gmail.com>
2021-12-02 15:43:54 +0200

    added base app

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import './style.css';
+
+let weather = {
+    apiKey: 'e3af2ea00e42cd06165f0bd4b145815d',
+    fetchWeather: function (city, unit) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${this.apiKey}`)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    fetchWeatherByCoords: function(long, lat, unit) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${this.apiKey}`)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector('.city').innerText = `Weather in ${name}`;
+        document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+        document.querySelector('.description').innerText = description;
+        document.querySelector('.temp').innerText = `${Math.floor(temp)}°C`
+        document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
+        document.querySelector('.wind').innerText = `Wind speed: ${speed} km/h`;
+        document.querySelector('.weather').classList.remove("loading");
+        document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${description.split(' ').join()})`;
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value, 'metric');
+        document.querySelector(".search-bar").value = "";
+    }
+}
+
+document.querySelector('.search button')
+        .addEventListener('click', function() {
+            weather.search();
+        });
+
+document.querySelector('.search-bar')
+        .addEventListener('keyup', function(event) {
+            if (event.key == "Enter") {
+                weather.search();
+            }
+        })
+
+navigator.geolocation.getCurrentPosition((positions) => {
+    const lat = positions.coords.latitude;
+    const long = positions.coords.longitude;
+    weather.fetchWeatherByCoords(long, lat, 'metric');
+})
